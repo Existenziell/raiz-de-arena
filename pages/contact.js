edfd0927132bad12f5864af03a53bb5825de5f0d
Christof <bauer.christof@gmail.com>
2022-08-02 19:30:09 -0500

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -12,8 +12,10 @@ const Contact = ({ i18n }) => {
 
       <div className='px-4 md:px-8 pt-12 pb-24'>
         <h1 className='text-6xl my-4'>{i18n.T1}</h1>
+        <div className='mt-20 flex flex-col justify-center bg-white shadow-2xl rounded-xl w-max px-8 py-6 mx-auto'>
+          <p className='text-brand-dark text-2xl mb-4'>Scan me</p>
 
-        <div className='mb-12 max-w-2xl mx-auto leading-relaxed text-xl text-left'>
+          <img src='/qr.jpg' alt='QR Code' className='w-44' />
         </div>
       </div>
     </>
